bfce50854609fda44c0784aeca066d25359e6bb5
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-20 23:41:19 +0900

    『投稿する』ボタンからの検索機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,16 @@ class ProductsController < RankingController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    #『like句』をwhereメソッドと共に使用。
+    #『（カラム名: バリュー）と記述して、レコードを取得』『where(user_id: @user.id)』
+
+    #下記の場合
+    # 前者："検索するカラム名 LIKE(?)"（書き方固定）
+    # 後者："検索するキーワード"
+    #      params内（URLで送られてくる）のkey『keyword』に対応するバリューに
+    #      曖昧文字列（%）を付ける＝『〜バリュー〜』（値）
+    @products = Product.where("title LIKE(?)", "%#{params[:keyword]}%").limit(20)
+  end
 end

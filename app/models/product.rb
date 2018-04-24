62a5cd3a3412dbb335a66a3bae82d6b956557228
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-25 00:48:19 +0900

    Revert "Indexページに、rateの平均点を算出・表示"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
 
   has_many  :reviews
+
+  def review_average
+    reviews.average(:rate).round
+    # self.reviews.average(:rate).round：上記の略さず記載の形
+      # 『self』を用いたのは、インスタンス変数productをそのまま記載するとエラーになる為。
+      # レシーバ（メソッドを使用したインスタンス自体の事）に対して適用することが出来る。
+  end
 end

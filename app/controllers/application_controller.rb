b3838acbded4a41cbc07baa3f39f20396a195a65
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-26 01:19:02 +0900

    サインアウト後のリダイレクト先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # サインアウト後のリダイレクト先を設定
+  # 引数には『resource』を渡す（deviseのメソッドを上書きしている為）
+  # 返り値にリダイレクト先のURLを指定
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
 end

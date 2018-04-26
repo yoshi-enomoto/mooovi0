9c90fddb9058af0c3c0209e8ebb4db48593caa58
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-26 19:18:41 +0900

    Revert "マイページの設定"
    This reverts commit c7076c4ba0ed449ec5048533e0e6529a7f6d7812.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
 
-  resources :users, only: [:show]
   resources :products, only: [:show] do
     resources :reviews, only: [:new, :create]
     collection do

9b31d4d7e18f2de722b2c3e934da8da51530e3ca
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-26 18:02:59 +0900

    nicknameカラムの追加＆gitignore"画像ファイル"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425140228) do
+ActiveRecord::Schema.define(version: 20180426090022) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",      limit: 255,   null: false
@@ -44,6 +44,11 @@ ActiveRecord::Schema.define(version: 20180425140228) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

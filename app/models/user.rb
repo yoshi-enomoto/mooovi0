c640a5e7ba323b3c09dd35f983fcbe91f1ab57fb
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-26 18:02:31 +0900

    Avatarカラムの定義＆画像のサイズ・バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # has_attached_file :カラム名,
+                    # styles:  { medium: "画像サイズ", thumb: "画像サイズ" }
+  # サイズを指定するための属性はstyles。
+  # stylesではどのような種類の画像をどの大きさで保存するか指定する。
+  # 「medium」や「thumb」はImageMagickで保存できる画像の種類です。
+  # 画像サイズの指定はImageMagickでの指定方法に準じる。
+  has_attached_file :avatar,
+                    styles:  { medium: "300x300#", thumb: "100x100#" }
+
+  # validates_attachment_content_type :カラム名,
+                                    # content_type: ["画像の種類"]
+  # ["画像の種類"]には、配列で指定する。
+  validates_attachment_content_type :avatar,
+                                    content_type: ["image/jpg","image/jpeg","image/png"]
+
+
+
 end

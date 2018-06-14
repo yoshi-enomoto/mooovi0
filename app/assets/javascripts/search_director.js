848f6d95a7646be8c8e7ad0ca092c841d224ea81
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-14 14:22:51 +0900

    入力値とそのアクション値の取得設定

--- app/assets/javascripts/search_director.js
+++ app/assets/javascripts/search_director.js
@@ -1,6 +1,10 @@
 $(document).on("turbolinks:load", function() {
 
   $("#search_directors").on("keyup", function() {
+    var input = $("#search_directors").val();
+    var url = $(this).parent().attr("action");
+    console.log(url);
+    console.log(input);
 
   });
 });

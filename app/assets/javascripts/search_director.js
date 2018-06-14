8a90292187741f50f68c07c9145731b35e761cd4
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-15 01:37:21 +0900

    呼び出す関数の詳細作成

--- app/assets/javascripts/search_director.js
+++ app/assets/javascripts/search_director.js
@@ -1,5 +1,48 @@
 $(document).on("turbolinks:load", function() {
 
+  var search_list = $(".listview.js-lazy-load-images");
+
+  function appendProduct(product) {
+    var html = `<li>
+                  <a class="listview__element--right-icon" href="/products/${product.id}/reviews/new" title="${product.title}">
+                    <div class="position-right p1em">
+                      <i class="icon-chevron-right color-sub">
+                      </i>
+                    </div>
+                    <div class="row no-space-bottom">
+                      <div class="col2">
+                        <div class="thumbnail thumbnail--movies">
+                          <div class="thumbnail__figure" style="background-image:url(${product.image});" title="${product.title}">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col6 push6">
+                        <h3 class="text-middle text-break">
+                          <span class="color-sub">
+                          ${product.title}
+                          </span>
+                          <p class="text-xsmall text-overflow">
+                          ${product.detail}
+                            <br><br>
+                            監督名：${product.director}
+                          </p>
+                        </h3>
+                      </div>
+                    </div>
+                  </a>
+                </li>`
+    search_list.append(html);
+  }
+
+  function appendNoProduct(product) {
+    var html = `<li>
+                  <div class="listview__element--right-icon">
+                  ${product}
+                  </div>
+                </li>`
+    search_list.append(html);
+  }
+
   $("#search_directors").on("keyup", function() {
     var input = $("#search_directors").val();
     var url = $(this).parent().attr("action");

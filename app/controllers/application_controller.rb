0bd8eae97b68738b5b17b567d90b068b7f2a8b53
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-15 01:38:26 +0900

    メモ書き追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   # 複数ページで部分テンプレートを使用時、呼び出しコントローラーによって表示内容を変える記述
   before_action :request_path
   def request_path
-      @path = controller_path + '#' + action_name
-      def @path.is(*str)
+      @path = controller_path + '#' + action_name #『controller_name』にも値が入っている
+      def @path.is(*str) #引数に『*』がある事で、その引数は配列となる。
           str.map{|s| self.include?(s)}.include?(true)
       end
   end
